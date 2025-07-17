ff2bc0c334ddb49cb0efc993952c6f97955a057d
Nehatkr <nehathakur2308@gmail.com>
2025-07-17 15:59:52 +0530

    Day12

--- seprate_an_array.js
+++ seprate_an_array.js
@@ -10,3 +10,24 @@ const character = input.filter(item => typeof item === 'string');
 
 console.log("Numbers: " , number);
 console.log("Character: " , character);
+
+
+// Separate Integers and Floats
+const arr2 = [1.2, 3, 4.5, 6, 7.0];
+
+const integers = arr2.filter(num => Number.isInteger(num));
+const floats = arr2.filter(num => !Number.isInteger(num));
+
+console.log("Integers:", integers); // [3, 6, 7]
+console.log("Floats:", floats);     // [1.2, 4.5]
+
+
+// Separate Alphanumeric and Special Characters
+
+const arr6 = ['a', '1', '%', 'b', '*', '2'];
+
+const alphanumeric = arr6.filter(char => /^[a-zA-Z0-9]$/.test(char));
+const special = arr6.filter(char => !/^[a-zA-Z0-9]$/.test(char));
+
+console.log("Alphanumeric:", alphanumeric); // ['a', '1', 'b', '2']
+console.log("Special Characters:", special); // ['%', '*']
