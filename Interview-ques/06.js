677fd0a81fb63e8627cf8ee5bf624edfa81953d0
Nehatkr <nehathakur2308@gmail.com>
2025-10-31 20:59:35 +0530

    Day45

--- Interview-ques/06.js
+++ Interview-ques/06.js
@@ -1,20 +1,38 @@
-const obj = { 
-  id: 1, 
-  details: { name: "Neha", skills: ["JS", "React"] }
+const obj = {
+  id: 1,
+  details: { name: "Neha", skills: ["JS", "React"] },
 };
-const { details: { name, skills: [s1] } } = obj;
+const {
+  details: {
+    name,
+    skills: [s1],
+  },
+} = obj;
 console.log(name, s1);
 
-
 // q2
-let a = 10, b = 20;
+let a = 10,
+  b = 20;
 [a, b] = [b, a];
 console.log(a, b);
 
 // q3
-const obj1 = { 
-  id: 1, 
-  details: { name1: "Neha", skills: ["JS", "React"] }
+const obj1 = {
+  id: 1,
+  details: { name1: "Neha", skills: ["JS", "React"] },
 };
-const { details: { name1, skills: [s0] } } = obj1;
-console.log(name2, s0);
+const {
+  details: {
+    name1,
+    skills: [s0],
+  },
+} = obj1;
+console.log(name1, s0);
+
+// print number from i to n
+let n = 5;
+for (let index = 1; index <= n; index++) {
+  console.log(n - index);
+}
+
+//
